b817f93717f39a4ffaf58ec91d46dafc93f5690e
Thejas <thejashc@gmail.com>
2016-06-26 16:12:26 +0200

    File Copies moved to new Folder
    SRD_PBC.f = Code for periodic boundary conditions with checks for momentum and energy conservation.
    SRD_lib_T.f = Library containing all modules like collision, wall-conditions, Probability distributions etc.
    SRD_work_T.f = MAIN() file for simulations
    file_writing_modules.f = Contains all codes pertaining to writing files for different situations
    velocity_averaging.f = Averages the velocity horizontal blocks for different time intervals after specified time steps.

---
+++ SRD_work_T.f
@@ -0,0 +1,82 @@
+      	PROGRAM SRD
+		use SRD_lib_T
+    		IMPLICIT NONE
+		
+		INTEGER, PARAMETER :: Gama=10,m=1,a0=1  ! Particle parameters
+		REAL, PARAMETER  :: kbt=1.0
+		INTEGER, PARAMETER :: Lx = 50,Ly = 50, l0=50, b0=50, width=10, msize = Lx*Ly
+		INTEGER, PARAMETER :: np=Gama*Ly*Lx, out_unit=20,  out_unit1=21, out_unit2=22, dp1 = selected_real_kind(15, 307)
+		
+		REAL :: av=0.0, std=sqrt(kbt/m), g=1e-4, dt_c=1.0, start, finish, seed
+		REAL (kind=dp1) :: alpha,fs = 7.0, twer
+		INTEGER :: tmax=90000, head(Ly,Lx), list(np), t, t_tot, t_count=0, p_count, i,j, ipar, rand_for_cell, counter = 0, fc_i, fc_j
+		REAL(kind=dp1) :: rx(np), ry(np), vx(np), vy(np), rx1(np), ry1(np), randposx(np), randposy(np), v1(np), v2(np), v3(np), v4(np)
+		REAL(kind=dp1) :: temp(Ly,Lx), temp_com(Ly,Lx), tempy(Ly,Lx), vxcom(Ly), vycom(Ly), vx1(Ly), vy1(Ly), vx1_temp(Ly), vy1_temp(Ly)
+		REAL(kind=dp1) :: momx_before_col(Ly,Lx), momx_after_col(Ly,Lx), momy_before_col(Ly,Lx), momy_after_col(Ly,Lx)  			
+		REAL(kind=dp1) :: ke_before_col(Ly,Lx), ke_after_col(Ly,Lx)
+		REAL(kind=dp1) :: momx_diff(Ly,Lx), momy_diff(Ly,Lx), ke_diff(Ly,Lx)
+		!REAL(kind=dp1) :: Tx(Ly,500000), 
+		LOGICAL  :: l1(np)
+		CHARACTER(LEN=1024)  :: fname
+		!real, dimension(:) , allocatable::rx1,ry1		! needed when square block is present
+		vxcom(Ly)=0.0
+		vycom(Ly)=0.0
+		vx1(Ly)=0.0
+		vy1(Ly)=0.0
+		ke_before_col = 0.0
+		ke_after_col = 0.0
+		momx_before_col = 0.0
+		momy_after_col = 0.0
+		momx_before_col = 0.0
+		momy_after_col = 0.0
+		
+		t_tot = tmax/dt_c
+		alpha = pi/2
+		call cpu_time(start)
+		
+		call initialize(rx,ry,vx,vy, Ly, Lx, np, av, std)	
+
+		!fs = 40.0
+			!write(*,*) factorial(i/2 -1)
+		!	twer = fs*log(fs)-fs + 0.5*log(2*pi*fs)+ log(1.0+1/(12*fs) + 1/(288*(fs**2)))
+		!	write(*,*) twer, factln(40)
+		
+		!write (fname, "(A14,I0.6)") "../Codes/initial_stats"                             
+    		!open (unit=out_unit,file='./data/mbs_E_fluc_b_1e3.dat',action="write",status="replace")
+    		!open (unit=out_unit1,file='./data/mbs_E_av_b_1e3.dat',action="write",status="replace")
+    			
+		do t=1,t_tot
+			
+			rx1 = rx + vx*dt_c + (g*dt_c**2)/2	! Streaming
+    			ry1 = ry + vy*dt_c  
+    			vx  = vx + g*dt_c			  
+    			
+    			call thermal_wall(rx, ry, rx1, ry1, vx, vy, Ly, dt_c, g, kbt,np)
+    			!call bounce_wall(rx, ry, rx1, ry1, vx, vy, Ly, dt_c, g, kbt,np)
+
+
+    			! Periodic Boundary condition in x
+    			l1 = (rx1<=0.0)      
+    			rx1 = mod(rx1,Lx*1.0)
+    			where (l1) 
+    				rx1 = rx1+Lx
+    			end where
+    			   
+			ry = ry1
+			rx = rx1
+    			
+			! Partitioning into cells
+    			call partition(rx,ry,head,list,Lx,Ly,np)      
+    						
+			call collision(vx, vy, temp, temp_com, tempy, head, list, Lx, Ly, alpha)	! Collision'		 		
+			
+    			! for averaging the values 
+ 
+		end do
+		
+
+		call cpu_time(finish)
+    		print '("Time = ",f10.3," seconds.")',finish-start
+    		
+
+      END PROGRAM SRD	
