66eeb9cbc519694c346159544d332ad8ce45312c
Thejas Hulikal Chakrapani <thejashc@gmail.com>
2018-02-04 20:18:33 +0100

    script for ifort and gcc compilers

--- script.sh
+++ script.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-rm -r *.o
-rm -r *.mod
+rm -f *.o
+rm -f *.mod
 
-ifort -fpp2 -FR -c -Tf SRD_lib.f95 
-ifort -fpp2 -FR -o SRD2D -Tf SRD_main.f95 SRD_lib.o
-
-
-
+if [[ $# -gt 0 ]]
+then
+	ifort -fpp2  -FR -c -Tf SRD2D_ifort_library.f95
+	ifort -fpp2  -FR -o SRD2D -Tf SRD2D_main.f95 SRD2D_ifort_library.o
+else
+	# gfortran -ffree-line-length-none -c SRD2D_gcc_library.f95 -fcheck=bounds
+	# gfortran -ffree-line-length-none -o SRD2D -c SRD2D_main.f95 SRD2D_gcc_library.o -fcheck=bounds
+	gfortran -ffree-line-length-none -c SRD2D_gcc_library.f95
+	gfortran -ffree-line-length-none -o SRD2D -c SRD2D_main.f95 SRD2D_gcc_library.o 
+fi
