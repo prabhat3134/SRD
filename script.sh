4bd489374ef12622cbb71b9e1f4da5c79545536c
Thejas Hulikal Chakrapani <thejashc@gmail.com>
2018-02-08 10:48:46 +0100

    script.sh with fcheck bounds

--- script.sh
+++ script.sh
@@ -8,8 +8,6 @@ then
 	ifort -fpp2  -FR -c -Tf SRD2D_ifort_library.f95
 	ifort -fpp2  -FR -o SRD2D -Tf SRD2D_main.f95 SRD2D_ifort_library.o
 else
-	# gfortran -ffree-line-length-none -c SRD2D_gcc_library.f95 -fcheck=bounds
-	# gfortran -ffree-line-length-none -o SRD2D -c SRD2D_main.f95 SRD2D_gcc_library.o -fcheck=bounds
-	gfortran -ffree-line-length-none -c SRD2D_gcc_library.f95
-	gfortran -ffree-line-length-none -o SRD2D -c SRD2D_main.f95 SRD2D_gcc_library.o 
+	gfortran -ffree-line-length-none -c SRD2D_gcc_library.f95 -fcheck=bounds
+	gfortran -ffree-line-length-none -o SRD2D SRD2D_main.f95 SRD2D_gcc_library.o -fcheck=bounds
 fi
