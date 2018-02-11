dea89f536b3720e5ff084c3aab98706a04e76845
prabhat3134 <pc.iitbbs@gmail.com>
2018-02-11 12:44:43 +0100

    parallel option added for compilation

--- script.sh
+++ script.sh
@@ -2,12 +2,13 @@
 
 rm -f *.o
 rm -f *.mod
-
-if [[ $# -gt 0 ]]
-then
+if [ $# -gt 1 ]; then
 	ifort -fpp2  -FR -c -Tf SRD2D_ifort_library.f95
 	ifort -fpp2  -FR -o SRD2D -Tf SRD2D_main.f95 SRD2D_ifort_library.o
+elif [ $# -gt 0 ]; then
+	gfortran -ffree-line-length-none -fopenmp -c SRD2D_gcc_library.f95 -fcheck=bounds
+	gfortran -ffree-line-length-none -fopenmp -o SRD2D_par SRD2D_main.f95 SRD2D_gcc_library.o -fcheck=bounds
 else
 	gfortran -ffree-line-length-none -c SRD2D_gcc_library.f95 -fcheck=bounds
-	gfortran -ffree-line-length-none -o SRD2D SRD2D_main.f95 SRD2D_gcc_library.o -fcheck=bounds
+	gfortran -ffree-line-length-none -o SRD2D_serial SRD2D_main.f95 SRD2D_gcc_library.o -fcheck=bounds
 fi
